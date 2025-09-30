8cdd7afd24ea91bac3eb6c0405e26e9860e54d3c
Blaine Jenkins <blaine.jenkins@empire.ca>
2025-09-30 12:09:30 -0400

    sdf sd fsdasd fds

---
+++ fixMSTeams.bat
@@ -0,0 +1,15 @@
+::THIS IS A QUICK AN EASY WAY TO FIX YOUR MS TEAMS ISSUE
+
+rmdir /Q /S C:\Users\%USERNAME%\appdata\roaming\Microsoft\teams\Cache
+rmdir /Q /S C:\Users\%USERNAME%\appdata\local\Microsoft\teams\Cache
+cd C:\Users\%USERNAME%\appdata\local\Microsoft\teams
+mkdir Cache
+cd C:\Users\%USERNAME%\appdata\roaming\Microsoft\teams
+mkdir Cache
+
+
+
+
+
+
+
